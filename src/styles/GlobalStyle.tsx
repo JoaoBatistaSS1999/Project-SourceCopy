4bfade3a4da700186b0e03540da661a6189c8d68
João Batista <jbsouzasantos1999@gmail.com>
2022-06-18 19:14:28 -0300

    global styles

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Roboto Mono", monospace;
+  color: #000000;
+  scroll-behavior: smooth;
+  transition: ease-in-out;
+  
+  &::selection {
+    background-color: #62ed17;
+    color: #ffffff;
+  }
+}
+
+button {
+    cursor: pointer;
+    border: none;    
+}
+
+ul {
+    list-style: none;
+}
+
+a {
+  text-decoration: none;  
+}
+
+h2 {
+  font-size: 22px;
+}
+
+
+    
+`;
